ce1b7d7094fd6528cc893b681a077a5728a40484
hw-erl-lnaylor <luke.naylor@h-partners.com>
2026-03-05 10:02:19 +0000

    Remove most of the scripts in public/js in favour of SSG with astro

---
+++ src/content/config.ts
@@ -0,0 +1,33 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.string(),
+    authors: z.array(z.string()),
+    repoLink: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    descriptionVideo: z.string().optional(),
+    descriptionImage: z.string().optional(),
+  }),
+});
+
+const news = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.string(),
+  }),
+});
+
+const pages = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    route: z.string(),
+  }),
+});
+
+export const collections = { blog, news, pages };
